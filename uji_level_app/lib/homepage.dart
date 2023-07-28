5e07b8bf0a276e038f07333692ed7f63e4be87f4
[Kiagusss] <[kiagus.muslim78@gmail.com]>
2023-07-28 15:35:26 +0700

    change tampilan dikit

--- uji_level_app/lib/homepage.dart
+++ uji_level_app/lib/homepage.dart
@@ -53,10 +53,8 @@ class _HomePageState extends State<HomePage> {
                     width: 330,
                     height: 180,
                     decoration: BoxDecoration(
-                      color: Color(
-                          0xff4894FE), 
-                      borderRadius: BorderRadius.circular(
-                          10),
+                      color: Color(0xff4894FE),
+                      borderRadius: BorderRadius.circular(10),
                     ),
                     child: Container(
                       margin: EdgeInsets.all(20),
@@ -65,34 +63,43 @@ class _HomePageState extends State<HomePage> {
                           Row(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                            CircleAvatar(
-                              backgroundImage: AssetImage("assets/pa riki.png"),
-                              radius: 35,
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(left: 15),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text("Mr.Ricky", style: GoogleFonts.poppins(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.w800
-                              ),),
-                              Text("General Teacher", style: GoogleFonts.poppins(
-                                color: Colors.white,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w400
-                              ),),
-                                ],
+                              CircleAvatar(
+                                backgroundImage:
+                                    AssetImage("assets/pa riki.png"),
+                                radius: 35,
                               ),
-
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(left: 20, top: 12),
-                              child: Icon(Icons.arrow_forward_ios, color: Colors.white, size: 30,),
-                            ),
-                          ],),
+                              Container(
+                                margin: EdgeInsets.only(left: 15),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Mr.Ricky",
+                                      style: GoogleFonts.poppins(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w800),
+                                    ),
+                                    Text(
+                                      "General Teacher",
+                                      style: GoogleFonts.poppins(
+                                          color: Colors.white,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w400),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(left: 20, top: 12),
+                                child: Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                              ),
+                            ],
+                          ),
                           Container(
                             margin: EdgeInsets.only(top: 20),
                             height: 1,
@@ -106,14 +113,20 @@ class _HomePageState extends State<HomePage> {
                                 Container(
                                   child: Row(
                                     children: [
-                                      Icon(Icons.calendar_month_outlined, color: Colors.white,),
+                                      Icon(
+                                        Icons.calendar_month_outlined,
+                                        color: Colors.white,
+                                      ),
                                       Padding(
-                                        padding: const EdgeInsets.only(left:5.0),
-                                        child: Text("Sunday, 12 June", style: GoogleFonts.poppins(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: 12
-                                        ),),
+                                        padding:
+                                            const EdgeInsets.only(left: 5.0),
+                                        child: Text(
+                                          "Sunday, 12 June",
+                                          style: GoogleFonts.poppins(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 12),
+                                        ),
                                       )
                                     ],
                                   ),
@@ -122,14 +135,20 @@ class _HomePageState extends State<HomePage> {
                                   margin: EdgeInsets.only(left: 40),
                                   child: Row(
                                     children: [
-                                      Icon(Icons.access_time_filled_rounded, color: Colors.white,),
+                                      Icon(
+                                        Icons.access_time_filled_rounded,
+                                        color: Colors.white,
+                                      ),
                                       Padding(
-                                        padding: const EdgeInsets.only(left:5.0),
-                                        child: Text("10:00 - 12:00", style: GoogleFonts.poppins(
-                                          color: Colors.white,
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.w500
-                                        ),),
+                                        padding:
+                                            const EdgeInsets.only(left: 5.0),
+                                        child: Text(
+                                          "10:00 - 12:00",
+                                          style: GoogleFonts.poppins(
+                                              color: Colors.white,
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.w500),
+                                        ),
                                       )
                                     ],
                                   ),
@@ -137,7 +156,6 @@ class _HomePageState extends State<HomePage> {
                               ],
                             ),
                           ),
-
                         ],
                       ),
                     ),
@@ -146,23 +164,59 @@ class _HomePageState extends State<HomePage> {
                     margin: EdgeInsets.only(top: 20),
                     width: 320,
                     child: TextFormField(
-                      
                       decoration: InputDecoration(
-                        hintText: "Search Anything Here!",
-                        prefixIcon: Icon(Icons.search_rounded, size: 30,),
-                         focusColor: Color(0xff4894FE),  
-                            filled: true,
-                            fillColor: Color.fromARGB(255, 247, 249, 253),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                        )
-                      ),
+                          hintText: "Search Anything Here!",
+                          prefixIcon: Icon(
+                            Icons.search_rounded,
+                            size: 30,
+                          ),
+                          focusColor: Color(0xff4894FE),
+                          filled: true,
+                          fillColor: Color.fromARGB(255, 247, 249, 253),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          )),
                     ),
-                  )
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 10),
+                        child: CircleAvatar(
+                          child: Image.asset("assets/sun-tbh.png"),
+                          radius: 30,
+                          backgroundColor: Color.fromARGB(255, 247, 249, 253),
+                        ),
+                      ),
+                      CircleAvatar(
+                        child: Image.asset("assets/sun-tbh.png"),
+                        radius: 30,
+                        backgroundColor: Color.fromARGB(255, 247, 249, 253),
+                      ),
+                      CircleAvatar(
+                        child: Image.asset("assets/sun-tbh.png"),
+                        radius: 30,
+                        backgroundColor: Color.fromARGB(255, 247, 249, 253),
+                      ),
+                      CircleAvatar(
+                        child: Image.asset("assets/sun-tbh.png"),
+                        radius: 30,
+                        backgroundColor: Color.fromARGB(255, 247, 249, 253),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
           ],
+        ),
+        floatingActionButton: Container(
+          width: 70,
+          height: 70,
+          child: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.calendar_month_outlined, size: 40),
+          ),
         ),
       ),
     );
